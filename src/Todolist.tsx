39a7cfc48954f5c3280f5241f6f9e186467a0b59
Iniok <ilidanilo@mail.ru>
2023-04-27 23:28:59 +0300

    01

---
+++ src/Todolist.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export type TaskPropsType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
+export type TodoPropsType = {
+    title: string
+    tasks: TaskPropsType[]
+}
+
+
+export const Todolist = (props: TodoPropsType): any => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(t => {
+                        return (
+                            <li>
+                                <input type={'checkbox'} checked={t.isDone}/>
+                                <span>{t.title}</span>
+                            </li>
+                        )
+                    }
+                )}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
