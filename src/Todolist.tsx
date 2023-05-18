e85b713c666df88a3e187d840e6b74d7bd1134a4
Iniok <ilidanilo@mail.ru>
2023-05-18 22:24:10 +0300

    06 0

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, KeyboardEventHandler, useState} from 'react';
 import {Button} from './Components/Button';
 
 export type TaskPropsType = {
@@ -13,9 +13,9 @@ export type TodoPropsType = {
     tasks: TaskPropsType[]
     removeTask: (todolistId: string, taskId: string) => void
     changeFilter: (todolistId: string, value: FilterType) => void
-    addTask: (todolistId: string,title: string) => void
-    changeTaskStatus: (todolistId: string , id: string, isDone: boolean) => void
-    removeTodolist: (todolistId:string) => void
+    addTask: (todolistId: string, title: string) => void
+    changeTaskStatus: (todolistId: string, id: string, isDone: boolean) => void
+    removeTodolist: (todolistId: string) => void
     filter: FilterType
 }
 
@@ -26,7 +26,6 @@ export const Todolist = (props: TodoPropsType) => {
 
     const [title, setTitle] = useState('')
     const [eror, setError] = useState<string | null>(null)
-    let [filterName, setFilterName] = useState<FilterType>('All')
 
 
     const addTaskHandler = () => {
@@ -38,17 +37,32 @@ export const Todolist = (props: TodoPropsType) => {
         }
     }
 
-    const changeFilterHandler = (value: FilterType) => {
-        props.changeFilter(props.todolistId, value)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler: React.KeyboardEventHandler = (e ) => {
+        setError(null)
+        if (e.key === 'Enter') {
+            addTaskHandler()
+        }
+
     }
 
     const removeTaskHandler = (todolistId: string, id: string) => {
         props.removeTask(todolistId, id)
     }
 
-    const removeTodolistHandler = (todolistId:string) => {
+    const removeTodolistHandler = (todolistId: string) => {
         props.removeTodolist(todolistId)
     }
+
+
+    const changeFilterHandler = (value: FilterType) => {
+        props.changeFilter(props.todolistId, value)
+    }
+
+
 
 
     return (
@@ -61,36 +75,30 @@ export const Todolist = (props: TodoPropsType) => {
             <div>
                 <input
                     value={title}
-                    onChange={e =>
-                        setTitle(e.currentTarget.value)}
-                    onKeyUp={e => {
-                        setError(null)
-                        if (e.key === 'Enter') {
-                            addTaskHandler()
-                        }
-                    }}
+                    onChange={onChangeHandler}
+                    onKeyUp={onKeyPressHandler}
                     className={eror ? 'error' : ''}
                 />
-                <Button callback={() => addTaskHandler()} name={'+'}/>
+                <button onClick={() => addTaskHandler()}>+</button>
                 {eror && <div className="error-message">{eror}</div>}
             </div>
+
+
             <ul>
                 {props.tasks.map(t => {
-
                         // const removeTaskHandler =() => {
                         //     props.removeTask(t.id)
                         // }
-
                         const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
                             let newIsDoneValue = e.currentTarget.checked;
-                            props.changeTaskStatus(props.todolistId , t.id, newIsDoneValue)
+                            props.changeTaskStatus(props.todolistId, t.id, newIsDoneValue)
                         }
 
                         return (
                             <li key={t.id} className={t.isDone ? 'is-done' : ''}>
                                 <input type={'checkbox'} checked={t.isDone} onChange={onChangeHandler}/>
                                 <span>{t.title}</span>
-                                <Button callback={() => removeTaskHandler(props.todolistId , t.id)} name={'X'}/>
+                                <button onClick={() => removeTaskHandler(props.todolistId, t.id)}>X</button>
                             </li>
                         )
                     }
